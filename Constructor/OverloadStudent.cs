9bd2b6f63a0218c117fc32ac5a1e5023060bf317
moshesc <beepbeep9860@gmail.com>
2018-06-05 06:55:55 +0100

    overloading constructor
    Like methods, a constructor can also be overloaded.
    overloaded constructors are differenet on the basis of their type of parameters or number of parameters.
    same like method overloading where you can have multiple method with the same name but different signature, with constructor overloading you can have multiple constructor with different signature, but only differencr is that constructor doesnt have return type.

---
+++ Constructor/OverloadStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    public class OverloadStudent
+    {
+        public OverloadStudent()
+        {
+            //Default constructor
+        }
+        public OverloadStudent(int age)
+        {
+            //single parameter constructor
+        }
+        public OverloadStudent(int age, string name)
+        {
+            //two parameter constuctor
+        }
+    }
+    
+}
--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -8,6 +8,12 @@ namespace Constructor
         {
             Student obj1 = new Student();
             Student obj2 = new Student(17,58.08F);
+
+            OverloadStudent obj3 = new OverloadStudent();//Defult Constructor
+            OverloadStudent obj4 = new OverloadStudent(43);//Single parameter
+            OverloadStudent obj5 = new OverloadStudent(37, "Steve");//Two parameters
+
+            Console.WriteLine(obj5);
         }
     }
 }
