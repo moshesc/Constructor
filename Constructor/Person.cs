cd9e148f6c50c06eb6de00bf1d959c53a9450d49
moshesc <beepbeep9860@gmail.com>
2018-06-05 06:20:24 +0100

    Commit

---
+++ Constructor/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    class Person
+    {
+      private  string LastName;
+       private string Firstname;
+    }
+    
+    public Person(string lastName, string firstName)
+    {
+        lastName = LastName;
+        firstName = firstName;
+
+    }
+}
